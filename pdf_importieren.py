7f804e7cea014ccfc29fb928886ce1162ece6967
LSServiceOperationsHRO <86047441+LSServiceOperationsHRO@users.noreply.github.com>
2021-06-17 16:43:37 +0200

    Create pdf_importieren.py
    Dieses Skript dient dem Laden von PDF-Dateien in das Python-Skript und der Extraktion des Textes aus der PDF-Datei
    
    Genutzt wird dafür das Python Modul "PyPDF2"

---
+++ pdf_importieren.py
@@ -0,0 +1,7 @@
+# Skript zum Laden einer, bisher noch im selben Ordner wie das Skript befindlichen, PDF-Datei
+# Anschlißend soll der Text aus der PDF-Datei extrahiert werden
+# Der Text wird anschließend untersucht hinsichtlich seiner Form und der Korrektheit der Extraktion
+# Weiterhin solll geprüft werden, ob nicht benötigte Textbausteine (Inhaltsverzeichnis/ Referenzen/...) bei der Extraktion entfernt werden können
+
+import PyPDF2
+
