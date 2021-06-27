e6a29e80d7bcaea7df7ed32bf55f513102a7c2ce
Lilian Do Khac <lilian.do.khac@rwth-aachend.de>
2021-06-27 19:23:36 +0200

    Changed path.

--- SCHATSI002.py
+++ SCHATSI002.py
@@ -1,7 +1,8 @@
 import os
 
-a = open("SCHATSI_included.txt", "w")
+a = open("data/output/SCHATSI_included.txt", "w")
 for path, subdirs, files in os.walk(r'/data/input'):
    for filename in files:
      f = os.path.join(path, filename)
      a.write(str(f) + os.linesep) 
+     
