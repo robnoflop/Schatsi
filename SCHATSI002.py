bd7032ae402f504a03f29e77090f3d738aa2aa97
Lilian Do Khac <lilian.do.khac@rwth-aachend.de>
2021-06-27 19:19:28 +0200

    Changed path.

--- SCHATSI002.py
+++ SCHATSI002.py
@@ -1,7 +1,7 @@
 import os
 
 a = open("SCHATSI_included.txt", "w")
-for path, subdirs, files in os.walk(r'C:\\Users\\dokha\\OneDrive\\Projects\\Literature Research\\SCHATSI\\Topic Modeling'):
+for path, subdirs, files in os.walk(r'/data/input'):
    for filename in files:
      f = os.path.join(path, filename)
      a.write(str(f) + os.linesep) 
