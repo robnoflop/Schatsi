ec06b6cde96749dd5f1e2e868bbe939e67193cb9
Moritz Momper <moritz.momper@gmail.com>
2021-06-27 19:57:06 +0200

    docker added

--- SCHATSI002.py
+++ SCHATSI002.py
@@ -1,8 +1,7 @@
 import os
 
-a = open("data/output/SCHATSI_included.txt", "w")
+a = open("/data/output/SCHATSI_included.txt", "w")
 for path, subdirs, files in os.walk(r'/data/input'):
-   for filename in files:
-     f = os.path.join(path, filename)
-     a.write(str(f) + os.linesep) 
-     
+    for filename in files:
+        f = os.path.join(path, filename)
+        a.write(str(f) + os.linesep)
