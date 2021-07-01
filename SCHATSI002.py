e0f6cee5ae6229384b6ec1d276e64e7165ed008b
LSServiceOperationsHRO <86047441+LSServiceOperationsHRO@users.noreply.github.com>
2021-07-01 12:31:11 +0200

    Update SCHATSI002.py
    This version of SCHATSI002 allows to search all pdf's in the input-folder and check if it's possible to extract the text from it, using the package "pdftotext".
    Every file will be checked and a line in the outputfile "SCHATSI_included.csv" will be written.
    
    The Outputfile will look like this:
    filename | type | included | excluded
    ------------------------------------------------
     abc.pdf  | pdf  |     X         |    __        <-- The text of this document could be extracted without any problems
     err.pdf    | pdf  |    __        |     X        <-- There was a problem and the document text could not be extracted,
            ...                                                     the files wont be included in the next steps 
    
    TO_DO:
    check the runtime of the whole process and track that into the file "SCHATSI_runtime.csv"

--- SCHATSI002.py
+++ SCHATSI002.py
@@ -1,7 +1,83 @@
 import os
+import csv
+import pdftotext
 
-a = open("/data/output/SCHATSI_included.txt", "w")
+
+# At first: open the Output-File --> "SCHATSI_included.csv"
+# local path for testing: "SCHATSI_included.csv"
+output = open("/data/output/SCHATSI_included.csv", 'w', newline='')
+# create a writer object, which is used to write the lines into the csv
+file = csv.writer(output, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+# onetime writing of a headline into the csv
+kopfzeile = ["filename", "type", "included", "excluded"]
+file.writerow(kopfzeile)
+
+
+# For all paths, subdirectories and files in the input-folder do:
+# local path for testing: "r'/home/h/Github/Testpdfs'"
 for path, subdirs, files in os.walk(r'/data/input'):
     for filename in files:
-        f = os.path.join(path, filename)
-        a.write(str(f) + os.linesep)
+
+        # with data path
+        g = os.path.join(path, filename)
+
+        # Filename in 'Schatsi_included.txt' schreiben, ohne den Dateipfad/ in csv-Datei
+        f = os.path.join(filename)
+
+        # try to read in a pdf and extract text from it
+        try:
+            with open(g, "rb") as pdffile:
+                # create an object which is filled with a raw byte stream, which contains the text from the pdf
+                pdf = pdftotext.PDF(pdffile)
+                # force python to turn the byte stream into a string
+                text = "\n\n".join(pdf)
+                # print the text into a terminal for testing
+                print(text)
+
+        # if it fails do:
+        # if there is an exception or an error, they will be catched and the file wont be included in the next steps
+        except:
+            if f.endswith(".pdf"):
+                datatype = "pdf"
+            elif f.endswith(".txt"):
+                datatype = "txt"
+            elif f.endswith(".csv"):
+                datatype = "csv"
+            elif f.endswith(".docx"):
+                datatype = "docx"
+            elif f.endswith(".odt"):
+                datatype = "odt"
+            else:
+                datatype = "unknown datatype"
+            zeile = [f, datatype, "__", "X"]
+
+        # if it succeeds do:
+        else:
+            if f.endswith(".pdf"):
+                datatype = "pdf"
+            elif f.endswith(".txt"):
+                datatype = "txt"
+            elif f.endswith(".csv"):
+                datatype = "csv"
+            elif f.endswith(".docx"):
+                datatype = "docx"
+            elif f.endswith(".odt"):
+                datatype = "odt"
+            else:
+                datatype = "unknown datatype"
+            zeile = [f, datatype, "X", "__"]
+
+        file.writerow(zeile)
+        # the outputfile will get a layout like this
+        """
+        filename | type | included | excluded
+        --------------------------------------
+        abc.pdf  | pdf  |     X    |    __        <-- The text of this document could be extracted without any problems
+        err.pdf  | pdf  |    __    |     X        <-- There was a problem and the document text could not be extracted,
+        ...                                           the files wont be included in the next steps 
+        ...
+        """
+
+output.close()
+
+
