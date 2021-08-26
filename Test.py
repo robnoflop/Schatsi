c3bb0f97afd5eb6f6936afa876af7685c2d3de00
HagenLast <hagen.last2@uni-rostock.de>
2021-08-26 16:57:15 +0200

    remove unnessessary files

--- Test.py
+++
@@ -1,6 +0,0 @@
-""" Diese Datei dient dem Testen verschiedener Git-Funktionen.
-Dem entsprechend kann diese Datei einfach ingoriert werden :-)
-"""
-
-print("Hallo lieber Leser, ich hoffe Dir geht es gut :-)")
-
