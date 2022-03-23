8d2478f0feb57bebee5a1491883679699a68aa11
LSServiceOperationsHRO <86047441+LSServiceOperationsHRO@users.noreply.github.com>
2022-03-23 09:44:05 +0100

    Update variables.py
    delete merge artifact

--- src/variables.py
+++ src/variables.py
@@ -11,9 +11,5 @@ SCHATSI_FUNCTIONAL_TERMS = os.path.join(SCHATSI_INPUT_FOLDER, "functional_terms.
 SCHATSI_RUNTIME = os.path.join(SCHATSI_OUTPUT_FOLDER, "schatsi_runtime.csv")
 
 # Static input
-<<<<<<< HEAD
 SCHATSI_STOPWORDS = os.path.join(path_prefix, "params/stopwords.csv")
-=======
-SCHATSI_STOPWORDS = os.path.join(path_prefix, "/params/stopwords.csv")
 
->>>>>>> 9e50e758a057d337c2536f473dcad0df681b506d
