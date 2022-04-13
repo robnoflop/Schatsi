6c12a96647d0a240b7d76ee4848f6bc1ba8c3fa4
HagenLast <hagen.last2@uni-rostock.de>
2022-04-13 19:36:21 +0200

    code fixes for counting words and ranking

--- src/variables.py
+++ src/variables.py
@@ -2,7 +2,14 @@ import os
 
 # User specific RUN_ID for aws session
 run_id = os.getenv('RUN_ID', default='')
-path_prefix = os.path.join("/data", run_id)
+if run_id == '':
+    # if docker is executed locally, there will be no run_id, and therefore we don't need a path on the cloud for the params
+    path_prefix = '/data'
+    print("RUN_ID is EMPTY")
+else:
+    # if docker is executed in the cloud, we define a path with a folder named 'data' in between
+    path_prefix = os.path.join("/data", run_id)
+    print("RUN_ID is NOT EMPTY")
 
 # User specific inputs and outputs
 SCHATSI_INPUT_FOLDER = os.path.join(path_prefix, "input")
@@ -11,5 +18,10 @@ SCHATSI_FUNCTIONAL_TERMS = os.path.join(SCHATSI_INPUT_FOLDER, "functional_terms.
 SCHATSI_RUNTIME = os.path.join(SCHATSI_OUTPUT_FOLDER, "schatsi_runtime.csv")
 
 # Static input
-SCHATSI_STOPWORDS = os.path.join(path_prefix, "params/stopwords.csv")
+if run_id == '':
+    SCHATSI_STOPWORDS = "params/stopwords.csv"
+else:
+    SCHATSI_STOPWORDS = os.path.join(path_prefix, "params/stopwords.csv")
+
+
 
