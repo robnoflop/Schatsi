4c17540188e0268882e7c98bf5486b9b08874aad
LSServiceOperationsHRO <86047441+LSServiceOperationsHRO@users.noreply.github.com>
2022-07-05 11:56:49 +0200

    Update variables.py
    Comments for better understanding and later usage

--- src/variables.py
+++ src/variables.py
@@ -1,3 +1,9 @@
+"""
+In This File  the paths of the files will be specified. This is nessesary because the files lie in a docker container, which uses a debian linux distro as base and local files from the user will be loaded into the container and local directories will be used as volumes, so the path must be specified
+
+In the moment SCHATSI can be used locally and in a cloud environment, so the input- and output paths varie.
+"""
+
 import os
 
 # User specific RUN_ID for aws session
@@ -12,13 +18,19 @@ else:
     print("RUN_ID is NOT EMPTY")
 
 # User specific inputs and outputs
+# This path contains the input files (i.e. the papers)
 SCHATSI_INPUT_FOLDER = os.path.join(path_prefix, "input")
+# this path specifies where the output of container
 SCHATSI_OUTPUT_FOLDER = os.path.join(path_prefix, "output")
+# the path for the functional_terms.csv
 SCHATSI_FUNCTIONAL_TERMS = os.path.join(SCHATSI_INPUT_FOLDER, "functional_terms.csv")
+# the path for the negative_terms.csv
 SCHATSI_NEGATIVE_TERMS = os.path.join(SCHATSI_INPUT_FOLDER,"negative_terms.csv")
+# the path for the runtime.csv
 SCHATSI_RUNTIME = os.path.join(SCHATSI_OUTPUT_FOLDER, "schatsi_runtime.csv")
 
 # Static input
+# in the moment there is only the "stopwords.csv" as a parameter file. But when other static input files will be needed /path_prefix/params/FILENAME is the right place for it
 if run_id == '':
     SCHATSI_STOPWORDS = "params/stopwords.csv"
 else:
