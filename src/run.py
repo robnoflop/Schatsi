b42c2d48ff386ff04c21fa3babd8434ec737ab87
Felix Schramm <felixschramm@live.de>
2023-08-16 17:48:27 +0200

    refactored run.py

--- src/run.py
+++ src/run.py
@@ -1,11 +1,10 @@
 import nltk
-
-nltk.download("punkt")
-nltk.download("stopwords")
-
 from schatsi.jobs import *
 
+# the punkt tokenizer model helps to split text into sentences
+# the stopwords corpus helps to identify words that are not relevant for the analysis
 nltk.download("punkt")
+nltk.download("stopwords")
 
 if __name__ == "__main__":
     job = ParallelJob("data/input", "data/output", "data/metadata/functional_terms.csv", "data/metadta/negative_terms.csv")
